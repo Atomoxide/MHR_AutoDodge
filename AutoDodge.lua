96bcdf74424ce954137b2d97f29225e9f63d475d
Ziyang Yuan <yuanzy.brandon2011@gmail.com>
2024-07-18 16:00:51 -0500

    implement AutoDodge

---
+++ AutoDodge.lua
@@ -0,0 +1,118 @@
+local masterPlayerIndex
+local masterPlayer
+local PlayerManager
+local action_id
+local last_id
+local userArmature
+local nodeID, last_nodeID
+local dodgeReady = true
+local jumpStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Jump"):get_data(nil)
+local wireJumpStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("WireJump"):get_data(nil)
+local escapeStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Escape"):get_data(nil)
+local damageStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Damage"):get_data(nil)
+local dodgeAction
+local shroudedVaultLock = false
+local dodgeActionNodeID = {
+    ["normal"] = 1731229352,
+    ["kijin_kyouka"] = 1902167730,
+    ["kijin"] = 2454049754
+}
+
+---- Check Player current Status
+sdk.hook(sdk.find_type_definition("snow.player.PlayerMotionControl"):get_method("lateUpdate"),
+function(args)
+	if not masterPlayer then return end
+    -- log.debug(tostring(dodgeReady))
+    ---- check player status
+    local isJump = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", jumpStateTag)
+	local isWireJump = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", wireJumpStateTag)
+	local isEscape = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", escapeStateTag)
+	local isDamage = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", damageStateTag)
+
+	---- check is weapon drawn
+	local weaponOn = masterPlayer:call("isWeaponOn")
+
+    ---- check shroudedVault
+    if nodeID == 3316282274 or nodeID == 3783600746 then
+        shroudedVaultLock = true
+    elseif nodeID ~= 2399642468 and nodeID ~= 3862130673 then
+        shroudedVaultLock = false
+    end
+	
+    
+    if not weaponOn then
+		dodgeReady = false
+		return
+	elseif (isJump or isWireJump or isEscape or isDamage) then
+		dodgeReady = false
+		return
+    elseif shroudedVaultLock then
+        dodgeReady = false
+        return
+	end
+
+    dodgeReady = true
+
+    local normal
+	local kijin
+    local state
+	normal = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Normal"):get_data(nil)
+	kijin = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Kijin"):get_data(nil)
+    state = masterPlayer:call("get_DBState")
+	kijinState = masterPlayer:call("isKijinKyouka")
+	if kijinState then
+		dodgeAction = dodgeActionNodeID["kijin_kyouka"]
+	elseif state == kijin then
+		dodgeAction = dodgeActionNodeID["kijin"]
+	elseif state == normal then
+		dodgeAction = dodgeActionNodeID["normal"]
+	else
+		dodgeAction = dodgeActionNodeID["normal"]
+	end
+    
+
+end,
+function(retval) return retval end
+)
+
+sdk.hook(sdk.find_type_definition("snow.player.PlayerQuestBase"):get_method("checkCalcDamage_DamageSide"),
+	function(args)
+		local manager = sdk.to_managed_object(args[2])	
+		local argManager = sdk.to_managed_object(args[3])
+		local damageData = argManager:call("get_AttackData")
+		
+		dmgOwnerType = damageData:call("get_OwnerType")
+		curPlayerIndex = manager:get_field("_PlayerIndex")
+		masterPlayerIndex = masterPlayer:get_field("_PlayerIndex")
+        -- log.debug("Damage detected")
+	end,
+    function(retval)
+		local dmgFlowType = sdk.to_int64(retval)
+		if curPlayerIndex == masterPlayerIndex and dmgOwnerType == 1 and dmgFlowType == 0 then
+			if dodgeReady then
+				masterPlayerBehaviorTree:call("setCurrentNode(System.UInt64, System.UInt32, via.behaviortree.SetNodeInfo)",dodgeAction,nil,nil)
+				return sdk.to_ptr(1)
+			else
+				return retval
+			end
+			return retval
+		end	
+		return retval
+	end
+)
+
+---- Hook player info
+local playerInputHook = playerInputHook or sdk.find_type_definition("snow.player.PlayerInput"):get_method("initCommandInfo()")
+sdk.hook(playerInputHook,
+function(args)
+	playerManager = playerManager or sdk.get_managed_singleton("snow.player.PlayerManager")
+	masterPlayer = playerManager:call("findMasterPlayer") -- snow.player.PlayerBase
+	if not masterPlayer then return end
+	masterPlayerIndex = masterPlayer:get_field("_PlayerIndex")
+	masterPlayerArmature = masterPlayer:getMotionFsm2() -- via.motion.MotionFsm2
+	nodeID = masterPlayerArmature:getCurrentNodeID(0) -- System.UInt64
+    masterPlayerGameObject = masterPlayer:call("get_GameObject") -- via.GameObject
+	masterPlayerBehaviorTree = masterPlayerGameObject:call("getComponent(System.Type)",sdk.typeof("via.behaviortree.BehaviorTree"))
+end,
+function(retval) return retval end
+)
--- test.lua
+++ test.lua
@@ -5,76 +5,124 @@ local action_id
 local last_id
 local userArmature
 local nodeID, last_nodeID
+local enableDodge = false
+local jumpStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Jump"):get_data(nil)
+local wireJumpStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("WireJump"):get_data(nil)
+local escapeStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Escape"):get_data(nil)
+local damageStateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("Damage"):get_data(nil)
 
--- sdk.hook(sdk.find_type_definition("snow.player.PlayerMotionControl"):get_method("lateUpdate"),
--- function(args)
--- 	local motionControl = sdk.to_managed_object(args[2])
--- 	local refPlayerBase = motionControl:get_field("_RefPlayerBase")
--- 	local curPlayerIndex = refPlayerBase:get_field("_PlayerIndex")
-	
--- 	if curPlayerIndex == masterPlayerIndex then
--- 		action_id = motionControl:get_field("_OldMotionID")
--- 		-- action_bank_id = motionControl:get_field("_OldBankID")
--- 	end
--- 	if action_id ~= last_id then
---         log.debug("action" .. tostring(action_id))
---         last_id = action_id
---     end
-	
--- 	if nodeID ~= last_nodeID then
--- 		log.debug("node: " .. tostring(nodeID))
--- 		last_nodeID = nodeID
--- 	end
-	
--- end,
--- function(retval) end
--- )
-
----- Check Player current Status
 
 sdk.hook(sdk.find_type_definition("snow.player.PlayerMotionControl"):get_method("lateUpdate"),
 function(args)
-	-- local dodgeTag
-	-- local isDodging
-	local state
-	local kijinState
-	-- dodgeTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("ContinueAction"):get_data(nil)
-	-- isDodging = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", dodgeTag)
-	-- isDodging = masterPlayer:call("checkWeaponDraw(System.Boolean)", true)
-	state = masterPlayer:call("get_DBState")
-	kijinState = masterPlayer:call("isKijinKyouka")
-	-- if isDodging then
-	-- 	log.debug("in state")
-	-- else
-	-- 	log.debug("nope")
-	-- end
-	local normal
-	local kijin
-	normal = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Normal"):get_data(nil)
-	kijin = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Kijin"):get_data(nil)
-	if kijinStateL then
-		log.debug("kijin kyouka")
-	elseif state == kijin then
-		log.debug("kijin")
-	elseif state == normal then
-		log.debug("normal")
-	else
-		log.debug("Unknown")
+	local motionControl = sdk.to_managed_object(args[2])
+	local refPlayerBase = motionControl:get_field("_RefPlayerBase")
+	local curPlayerIndex = refPlayerBase:get_field("_PlayerIndex")
+	
+	if curPlayerIndex == masterPlayerIndex then
+		action_id = motionControl:get_field("_OldMotionID")
+		-- action_bank_id = motionControl:get_field("_OldBankID")
+	end
+	if action_id ~= last_id then
+        -- log.debug("action" .. tostring(action_id))
+        last_id = action_id
+    end
+	
+	if nodeID ~= last_nodeID then
+		log.debug("node: " .. tostring(nodeID))
+		last_nodeID = nodeID
 	end
 	
 end,
 function(retval) end
 )
 
-re.on_frame(function()
-	if not PlayerManager then
-        PlayerManager = sdk.get_managed_singleton('snow.player.PlayerManager')
-    end
-	if not PlayerManager then return end
-	masterPlayer = PlayerManager:call("findMasterPlayer")
-    if not masterPlayer then return end
-	masterPlayerIndex = masterPlayer:get_field("_PlayerIndex")
-	userArmature = masterPlayer:getMotionFsm2()
-	nodeID = userArmature:getCurrentNodeID(0)
+
+---- Check Player current Status
+-- sdk.hook(sdk.find_type_definition("snow.player.PlayerMotionControl"):get_method("lateUpdate"),
+-- function(args)
+-- 	if not masterPlayer then return end
+
+-- 	---- check is weapon drawn
+-- 	local weaponOn = masterPlayer:call("isWeaponOn")
+-- 	if not weaponOn then
+-- 		dodgeReady = false
+-- 		return
+-- 	end
+
+-- 	local isJump = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", jumpStateTag)
+-- 	local isWireJump = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", wireJumpStateTag)
+-- 	local isEscape = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", escapeStateTag)
+-- 	local isDamage = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", damageStateTag)
+
+-- 	if isJump or isWireJump or isEscape or isDamage then
+-- 		dodgeReady = false
+-- 		return
+-- 	end
+
+-- 	local stateTag
+-- 	local isInState
+-- 	-- local state
+-- 	-- local kijinState
+
+-- 	---- check snow.player.ActionNoAttack
+-- 	-- stateTag = sdk.find_type_definition("snow.player.ActionNoAttack"):get_field("Attack000"):get_data(nil)
+-- 	-- isInState = masterPlayer:call("isActionNoAttackTag(snow.player.ActionNoAttack)", stateTag)
+
+-- 	---- check snow.player.ActStatus
+-- 	stateTag = sdk.find_type_definition("snow.player.ActStatus"):get_field("WireJump"):get_data(nil)
+-- 	isInState = masterPlayer:call("isActionStatusTag(snow.player.ActStatus)", stateTag)
+
 	
-end)
+
+-- 	-- isInState = masterPlayer:call("checkWeaponDraw(System.Boolean)", true)
+-- 	-- state = masterPlayer:call("get_DBState")
+-- 	-- kijinState = masterPlayer:call("isKijinKyouka")
+-- 	if isInState then
+-- 		log.debug("in state")
+-- 	else
+-- 		log.debug("nope")
+-- 	end
+-- 	-- local normal
+-- 	-- local kijin
+-- 	-- normal = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Normal"):get_data(nil)
+-- 	-- kijin = sdk.find_type_definition("snow.player.DualBlades.DualBladesState"):get_field("Kijin"):get_data(nil)
+-- 	-- if kijinState then
+-- 	-- 	log.debug("kijin kyouka")
+-- 	-- elseif state == kijin then
+-- 	-- 	log.debug("kijin")
+-- 	-- elseif state == normal then
+-- 	-- 	log.debug("normal")
+-- 	-- else
+-- 	-- 	log.debug("Unknown")
+-- 	-- end
+	
+-- end,
+-- function(retval) return retval end
+-- )
+
+---- Hook player info
+local playerInputHook = playerInputHook or sdk.find_type_definition("snow.player.PlayerInput"):get_method("initCommandInfo()")
+sdk.hook(playerInputHook,
+function(args)
+	playerManager = playerManager or sdk.get_managed_singleton("snow.player.PlayerManager")
+	masterPlayer = playerManager:call("findMasterPlayer") -- snow.player.PlayerBase
+	if not masterPlayer then return end
+	masterPlayerIndex = masterPlayer:get_field("_PlayerIndex")
+	userArmature = masterPlayer:getMotionFsm2() -- via.motion.MotionFsm2
+	nodeID = userArmature:getCurrentNodeID(0) -- System.UInt64
+end,
+function(retval) return retval end
+)
+
+-- re.on_frame(function()
+-- 	if not PlayerManager then
+--         PlayerManager = sdk.get_managed_singleton('snow.player.PlayerManager')
+--     end
+-- 	if not PlayerManager then return end
+-- 	masterPlayer = PlayerManager:call("findMasterPlayer")
+--     if not masterPlayer then return end
+-- 	masterPlayerIndex = masterPlayer:get_field("_PlayerIndex")
+-- 	userArmature = masterPlayer:getMotionFsm2()
+-- 	nodeID = userArmature:getCurrentNodeID(0)
+	
+-- end)
